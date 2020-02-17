b3862365fab9d30608d15224a59c55c1f98d7b19
matt johannsen <majohann@hotmail.com>
2020-02-17 14:59:04 -0600

    Refined the name generating function. I am ready to start playing with the Store functions.

--- Customer.cs
+++ Customer.cs
@@ -17,10 +17,13 @@ namespace LemonadeStand_3DayStarter
         public Customer(Random rnd)
         {
             this.rnd = rnd;
-            names = new List<string>() { "Matt", "Chris", "Joe", "Mary", "Christine", "Jennifer", "Jeff", "Julie", "Mark", "Mike", "Bob", "Bill", "Paul", "Rachel", "Monica", "Chandler", "Ross", "Courtney", "Cory", "Brandon", "Casey", "Lindsay", "Melissa", "Tiffany", "Renae", "Jason", "Scott", "Reggie", "Creed", "Jim", "John", "Carol", "Pat", "Jordan", "Sue", "Don", "Linda", "Corbin", "Sarah" };
+            names = new List<string>() { "Matt", "Chris", "Joe", "Mary", "Christine", "Jennifer", "Jeff", "Julie", "Mark", "Mike", "Bob", "Bill", "Paul", "Rachel", "Monica", "Chandler", "Ross", "Courtney", "Cory", "Brandon", "Casey", "Lindsay", "Melissa", "Tiffany", "Renae", "Jason", "Scott", "Reggie", "Creed", "Jim", "John", "Carol", "Pat", "Jordan", "Sue", "Don", "Linda", "Corbin", "Sarah", "Sam", "Marcus", "Violet", "Alex", "Jarrett", "Jared", "Heather", "Brianna", "Megan", "Jamie" };
             int result;
             result = rnd.Next(0, names.Count);
+            //Console.WriteLine(names.Count + " " + rnd);
+            //string name = "matt";
             string name = names[result];
+            Console.WriteLine(name);
         }
 
         //member methods
